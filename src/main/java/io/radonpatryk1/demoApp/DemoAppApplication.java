dc4f0ee357390eeffd1b305ee23f44457bd28375
Patryk Radon <radonpatryk1@gmail.com>
2023-11-08 21:34:20 +0100

    test1

--- src/main/java/io/radonpatryk1/demoApp/DemoAppApplication.java
+++ src/main/java/io/radonpatryk1/demoApp/DemoAppApplication.java
@@ -11,12 +11,26 @@ public class DemoAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoAppApplication.class, args);
+
+		int n = rekurencja(5);
+
+		System.out.println(n);
 	}
+
 
 
 	@Bean
 	Validator validator() {
 		return new LocalValidatorFactoryBean();
+	}
+
+	public static int rekurencja (int i) {
+
+		int result = 0;
+		for (int j = 0; j < i+1; j++) {
+			result = result + i;
+		}
+		return result;
 	}
 
 }
